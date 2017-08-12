7811a1ce80f405228947edf2d243c576bb745725
Kevin Genus <kevin.genus@gmail.com>
2017-08-11 20:20:55 -0400

    updates model/index.js, api/index.js, sets database to 'ecorpse'

--- ecServer/server/db/db.js
+++ ecServer/server/db/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize')
 const db = new Sequelize(
-  process.env.DATABASE_URL || 'postgres://localhost:5432/ecorp', {
+  process.env.DATABASE_URL || 'postgres://localhost:5432/ecorpse', {
     logging: false
   }
 )
--- ecServer/server/db/models/index.js
+++ ecServer/server/db/models/index.js
@@ -4,24 +4,24 @@ const Assignment = require('./assignments')
 const Like = require('./likes')
 const Photo = require('./photos')
 
-Photo.belongsTo(Corpse)
+User.hasMany(Corpse, {onDelete: 'CASCADE'})
+User.hasMany(Photo, {onDelete: 'CASCADE'})
+User.hasMany(User, {as: 'Friends'})
+User.hasMany(Like, {onDelete: 'CASCADE'})
+
+Corpse.belongsTo(User)
+Corpse.hasMany(Photo, {onDelete: 'CASCADE'})
+Corpse.hasMany(Like, {onDelete: 'CASCADE'})
+
 Photo.belongsTo(User)
+Photo.belongsTo(Corpse)
+Like.belongsTo(User)
+Like.belongsTo(Corpse)
 
 Assignment.belongsTo(Corpse)
 Assignment.belongsTo(User, {as: 'assignorId'})
 Assignment.belongsTo(User, {as: 'assigneeId'})
 
-Corpse.hasMany(Photo, {onDelete: 'CASCADE'})
-Corpse.hasMany(Likes, {onDelete: 'CASCADE'})
-
-Like.belongsTo(User)
-Like.belongsTo(Corpse)
-
-User.hasMany(Photo, {onDelete: 'CASCADE'})
-User.hasMany(Corpse, {onDelete: 'CASCADE'})
-User.hasMany(Like, {onDelete: 'CASCADE'})
-
-User.hasMany(Users, {as: 'friends'})
 
 module.exports = {
   User,
