8e589a09d5ef01d9d7d91479f4be945771a97eac
Fara Woolf <fbwoolf@gmail.com>
2017-08-17 22:19:46 -0500

    feat: add edge to camera

--- components/User/UserEdges.js
+++ components/User/UserEdges.js
@@ -1,42 +1,65 @@
 import React, { Component } from 'react'
-import { StyleSheet, Text, ScrollView, View, Image } from 'react-native'
+import { StyleSheet, Text, ScrollView, View, Image, TouchableHighlight } from 'react-native'
+import styles from '../Style/UserHomeStyles'
+import {connect} from 'react-redux'
+import { fetchCorpes } from '../../store'
+import { imageUrl } from '../../store/url'
 
-export default class UserEdges extends Component {
+class UserEdges extends Component {
+  componentDidMount () {
+    this.props.fetchData()
+  }
+
   render () {
-    const baseUri = 'https://battleoftheships.herokuapp.com/images/corpse'
+    const { corpses } = this.props
+    const navigate = this.props.navigation.navigate
 
     return (
-      <View style={styles.container}>
-        <ScrollView>
-          {[1, 2].map((index) => {
-            return (
-              <View key={index}>
-                <Image
-                  style={styles.corpseEdge}
-                  source={{uri: `${baseUri}${index}/1.jpg`}}
-                  resizeMode='cover'
-                />
-              </View>)
-          })}
-        </ScrollView>
-      </View>
+      <ScrollView>
+        <View style={styles.container}>
+          {
+            corpses.map(corpse => {
+              if (corpse.id === 3) {
+                return (
+                  <View key={corpse.id} style={styles.corpse}>
+                    <View style={styles.imageCorpseTop}>
+                      <Text style={styles.textCorpse}>{corpse.photos.map((photo, i) => { return photo.user.username }).join('|')}</Text>
+                      <Text style={styles.titleCorpse}>{corpse.title}</Text>
+                      <Text style={styles.textCorpse}>...</Text>
+                    </View>
+                    <View style={styles.viewCorpse}>
+                      {corpse.photos.map((photo) => {
+                        return (
+                          <TouchableHighlight key={photo.id} onPress={() => { navigate('EdgeCameraScreen') }}>
+                            <Image
+                              style={styles.imageCorpse}
+                              source={{uri: `${imageUrl}/${photo.edgeUrl}`}}
+                            />
+                          </TouchableHighlight>
+                        )
+                      }).reverse()}
+                    </View>
+                  </View>
+                )
+              }
+            })
+          }
+        </View>
+      </ScrollView>
     )
   }
 }
 
-UserEdges.navigationOptions = ({ navigation }) => ({
-  title: 'Edges'
-})
+const mapStateToProps = (state) => {
+  return {
+    corpses: state.corpses
+  }
+}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center'
-  },
-  corpseEdge: {
-    height: 120,
-    width: 360,
-    marginTop: 15,
-    marginBottom: 15
+const mapDispatchToProps = (dispatch) => ({
+  fetchData: () => {
+    dispatch(fetchCorpes())
   }
 })
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserEdges)
