44c0b4bacb3f00bf8cb28d7d0ce93679cbec3d36
Fara Woolf <fbwoolf@gmail.com>
2017-08-19 20:04:05 -0500

    fix: change urls back

--- store/url.js
+++ store/url.js
@@ -1,5 +1,2 @@
-// export const apiUrl = 'https://exquisitecorpse-fsa.herokuapp.com/api'
+export const apiUrl = 'https://exquisitecorpse-fsa.herokuapp.com/api'
 export const imageUrl = 'https://s3.amazonaws.com/exquisitecorpse-s3-001/'
-
-export const apiUrl = 'http://192.168.7.27:8080/api'
-// export const imageUrl = 'https://192.168.7.27:8080/images'
