794747862bc095a4a4ac072f1850b90d1c70155c
Lina Jones <lina.jones630@gmail.com>
2017-08-23 17:42:48 -0600

    (fix) restore button position

--- components/Camera/NewCorpse.js
+++ components/Camera/NewCorpse.js
@@ -103,7 +103,7 @@ const styles = StyleSheet.create({
   options: {
     position: 'absolute',
     flexDirection: 'row',
-    bottom: 20,
+    bottom: 40,
     left: 0,
     right: 0,
     justifyContent: 'center'
