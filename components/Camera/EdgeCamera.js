da733e0e89bcdf2f19220164a7436a30635fa748
Fara Woolf <fbwoolf@gmail.com>
2017-08-23 15:41:05 -0500

    feat: remove photo from NewCorpse

--- components/Camera/EdgeCamera.js
+++ components/Camera/EdgeCamera.js
@@ -51,14 +51,6 @@ const mapDispatchToProps = (dispatch, ownProps) => ({
 export default connect(mapStateToProps, mapDispatchToProps)(EdgeCamera)
 
 const styles = StyleSheet.create({
-  capture: {
-    flex: 0,
-    backgroundColor: '#fff',
-    borderRadius: 5,
-    color: '#000',
-    padding: 10,
-    margin: 40
-  },
   container: {
     flex: 1,
     flexDirection: 'row'
